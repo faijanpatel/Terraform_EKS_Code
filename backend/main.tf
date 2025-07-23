c6b888b8ada942464805ae27f18cdd40250a71db
faijanpatel <faijanpatel@gmail.com>
2025-07-22 20:29:50 -0500

    first commit

---
+++ backend/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-eks-faijan-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "terraform-eks-statelock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+}
