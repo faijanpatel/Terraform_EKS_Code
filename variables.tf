7fa8ba494b8b2d2aa18eed6491c035f40fc7881b
Faijan Patel <30648619+faijanpatel@users.noreply.github.com>
2025-08-24 11:06:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-2"
 }
 
 variable "vpc_cidr" {
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-east-2", "us-east-2", "us-east-2"]
 }
 
 variable "private_subnet_cidrs" {
